e4ecc9ec8d4418859ad9cd50f625509e1fc5c410
shashi8verma <sv88166@gmail.com>
2024-09-29 18:35:47 +0530

    Commit on 29-09-2024

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+
+export const routes: Routes = [
+    { path: 'first', component: FirstComponent },
+    { path: 'second', component: SecondComponent },
+];
